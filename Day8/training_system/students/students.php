6f346a9bd088646d787a2ba3513894854ee36b73
shimaamohamed14 <shimaasadka4@gmail.com>
2025-07-23 02:56:48 +0300

    Add files via upload

---
+++ Day8/training_system/students/students.php
@@ -0,0 +1,43 @@
+<?php
+include '../db/db.php';
+?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<body class="">
+
+    <?php include '../navbar.php'; ?>
+    <div class="container p-5">
+    <h2>Student List</h2>
+    <a href="add_student.php" class="btn btn-success mb-3">Add Student</a>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>DOB</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $q = "SELECT * FROM students";
+            $res = mysqli_query($conn, $q);
+            while ($row = mysqli_fetch_assoc($res)) {
+                echo "<tr>
+                <td>{$row['name'] }</td>
+                <td>{$row['email']}</td>
+                <td>{$row['phone']}</td>
+                <td>{$row['date_of_birth']}</td>
+                <td>
+                            <a href='edit_student.php?id={$row['id']}' class='btn btn-warning'>Edit</a>
+                            <a href='delete_student.php?id={$row['id']}' class='btn btn-danger'>Delete</a>
+                          </td>";
+                    echo "</tr>";
+                }
+           
+            ?>
+        </tbody>
+    </table>
+    </div>
+</body>
+
