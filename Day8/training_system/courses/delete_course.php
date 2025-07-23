66309de41acd34a46768f5879835b16c4485c94c
shimaamohamed14 <shimaasadka4@gmail.com>
2025-07-23 03:02:41 +0300

    Delete Day8/training_system directory

--- Day8/training_system/courses/delete_course.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../db/db.php';
-
-$id = $_GET['id'];
-$q = "DELETE FROM courses WHERE id='$id'";
-if (mysqli_query($conn, $q)) {
-    header('Location: courses.php');
-}
--- Day8/training_system/db/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn =mysqli_connect("localhost","root","","training_system");
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
